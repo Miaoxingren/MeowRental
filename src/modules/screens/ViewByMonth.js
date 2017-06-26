abc00d4c103c91e2a1b69d443e0a82a01672a33b
Miaoxingren <363326042@qq.com>
2017-06-26 10:34:28 +0800

    view by month

---
+++ src/modules/screens/ViewByMonth.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {Text, View, FlatList} from 'react-native';
+
+import {connect} from 'react-redux';
+
+import ViewItem from '../components/ViewItem';
+import {ItemSeparator} from '../components/Common';
+
+import styles from '../styles/View';
+
+class ViewByMonthScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderItem({item}) {
+        let {title, rented, rental} = item;
+        return (<ViewItem title={title} rented={rented} rental={rental}/>);
+    }
+
+    renderHeader() {
+        let {date} = this.props;
+        return (
+            <View>
+                <Text>{date}</Text>
+            </View>
+        )
+    }
+
+    render() {
+        let {data} = this.props;
+        return (
+            <View style={styles.container}>
+                <FlatList data={data}
+                    renderListHeader={this.renderListHeader.bind(this)}
+                    renderItem={this.renderItem.bind(this)}
+                    ItemSeparatorComponent={ItemSeparator}/>
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(state, ownProps) {
+    return {};
+}
+
+export default connect(mapStateToProps)(ViewByMonthScreen);
