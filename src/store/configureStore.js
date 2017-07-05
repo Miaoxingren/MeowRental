84ea57370b16f03297e99bf6f7bfd07f301a0c95
Miaoxingren <363326042@qq.com>
2017-07-05 22:19:39 +0800

    add redux-persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,22 +1,16 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
-let middleware = [thunk];
+import {autoRehydrate} from 'redux-persist'
 
-if (__DEV__) {
-	const reduxImmutableStateInvariant = require('redux-immutable-state-invariant').default();
-	middleware = [...middleware, reduxImmutableStateInvariant, logger];
-} else {
-	middleware = [...middleware];
-}
+let middleware = [autoRehydrate(), thunk];
 
 export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
 		initialState,
-		applyMiddleware(...middleware)
+		autoRehydrate()
 	);
 }
