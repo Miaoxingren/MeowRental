4efa0e0b102dce247df2507f946246cff46886d5
Miaoxingren <363326042@qq.com>
2017-07-06 15:30:45 +0800

    remove thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,8 @@
 
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
 import {autoRehydrate} from 'redux-persist'
-
-let middleware = [autoRehydrate(), thunk];
 
 export default function configureStore(initialState) {
 	return createStore(
